0f10d0a3766d2da65eff47d71745d9f92e2953dc
munishbansal29 <munish0029@gmail.com>
2022-01-15 14:37:08 +0530

    adding screenshots

--- frontend/src/app/containers/Employee/services/employees.ts
+++ frontend/src/app/containers/Employee/services/employees.ts
@@ -1,4 +1,4 @@
-import { HttpService } from '../../../services/httpService2';
+import { HttpService } from '../../../services/httpService';
 import { EmployeeModel, 
   EmployeeUpdateStringFieldModel, 
   EmployeeUpdateDateFieldModel,
